3c81d8400033fbbfb2e8fc22223478d413d9286c
Jamaica2.1 <83459332+Jamaica13@users.noreply.github.com>
2022-09-12 16:54:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+const intents = new Discord.IntentsBitField(3276799)
+const bot = new Discord.Client({intents})
+const loadCommands = require("./Loaders/loadCommands")
+const loadEvents = require("./Loaders/loadEvents")
+const config = require("./config")
+
+bot.commands = new Discord.Collection()
+
+bot.color = "#7289da";
+
+bot.login(config.token)
+loadCommands(bot)
+loadEvents(bot)
